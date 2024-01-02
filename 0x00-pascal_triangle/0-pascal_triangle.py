c5500d041f96dc1ba867601c39e2d31fbda61203
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-02 14:39:09 +0200

    Remove redundant if statement

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,12 +11,11 @@ def pascal_triangle(n: int) -> list[list[int]]:
     if n <= 0:
         return triangle
     triangle.append([1])
-    if n >= 2:
-        for i in range(1, n):
-            row = [1]
-            for j in range(len(triangle[-1])-1):
-                row.append(triangle[-1][j]+triangle[-1][j+1])
-            row.append(1)
-            triangle.append(row)
+    for i in range(1, n):
+        row = [1]
+        for j in range(len(triangle[-1])-1):
+            row.append(triangle[-1][j]+triangle[-1][j+1])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
