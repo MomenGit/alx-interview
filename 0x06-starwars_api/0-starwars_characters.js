d0f9aad393013d2f95f57fd03a916d33626cb046
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-13 22:33:53 +0200

    Fix add reserving order of characters as they are in the endpoint

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,17 +3,26 @@
 const request = require('request');
 const process = require('process');
 
+const movieId = process.argv[2];
+
+function fetchCharacters (characterUrls, index) {
+  if (index >= characterUrls.length) return;
+
+  request.get(characterUrls[index], (err, res, body) => {
+    if (err) return;
+    const character = JSON.parse(body);
+    console.log(character.name);
+    fetchCharacters(characterUrls, index + 1);
+  });
+}
+
 request.get(
-  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  `https://swapi-api.alx-tools.com/api/films/${movieId}`,
   (err, res, body) => {
     if (err) console.log(err);
     else {
       const charactersUrls = JSON.parse(body).characters;
-      charactersUrls.forEach((url) => {
-        request.get(url, (err, res, body) => {
-          if (!err) console.log(JSON.parse(body).name);
-        });
-      });
+      fetchCharacters(charactersUrls, 0);
     }
   }
 );
